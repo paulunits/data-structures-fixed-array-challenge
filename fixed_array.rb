3e86615b9eef74d08c28e5767f11e0a78d8d4e55
paulunits <obsolete.units@gmail.com>
2016-08-03 10:24:20 -0700

    Add Fixed Array and Array List class methods

--- fixed_array.rb
+++ fixed_array.rb
@@ -24,3 +24,44 @@ class FixedArray
 	end		
 
 end
+
+class ArrayList
+
+	#Instantiate a new dynamic array with initial size size.
+	def initialize(size)
+		@size = size
+		@data = Array.new(size)
+	end	
+
+	#Add element to the end of the list. Return the element added.
+	def add(element)
+		@size += 1
+		@data.push(element)
+		element
+	end	
+
+	#Retrieve an element at index. If no element exists at that index, throw an OutOfBoundsException
+	def get(index)
+		raise OutOfBoundsException if index >= @size
+		@data[index]
+	end
+
+	#Replace an existing element at index. Return the element. If no element exists at that index, 
+	#throw an OutOfBoundsException
+
+	def set(index, element)
+		raise OutOfBoundsException if index >= @size
+		@data[index] = element
+	end
+	
+	#Return the number of items in the list
+	def length
+		@data.length
+	end
+	
+	def insert(index, element)
+		raise OutOfBoundsException if index >= @size
+		@data.insert(index, element)
+	end	
+
+end			
